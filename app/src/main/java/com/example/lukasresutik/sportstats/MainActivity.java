1b0d588ddbcfa30a54965ea9409860d5c9a0ff23
Lukáš Rešutík <lresutik@gmail.com>
2017-08-21 13:46:27 +0200

    Add listener
    Add listeners and map view components

--- app/src/main/java/com/example/lukasresutik/sportstats/MainActivity.java
+++ app/src/main/java/com/example/lukasresutik/sportstats/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.lukasresutik.sportstats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button bLogin;
+    private TextView registerLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 
+    private void init() {
+        etUsername = (EditText) findViewById(R.id.etUserName);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.btLogin);
+        registerLink = (TextView) findViewById(R.id.tvRegisterHere);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent registerIntent = new Intent(MainActivity.this,RegisterActivity.class);
+        MainActivity.this.startActivity(registerIntent);
+    }
 }
--- app/src/main/java/com/example/lukasresutik/sportstats/RegisterActivity.java
+++ app/src/main/java/com/example/lukasresutik/sportstats/RegisterActivity.java
@@ -2,12 +2,29 @@ package com.example.lukasresutik.sportstats;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText etUsername;
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etPassword2;
+    private Button bRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        init();
+    }
+
+    private void init() {
+        etUsername = (EditText) findViewById(R.id.etUserName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etPassword2 = (EditText) findViewById(R.id.etPassword2);
+        bRegister = (Button) findViewById(R.id.btRegister);
     }
 }
