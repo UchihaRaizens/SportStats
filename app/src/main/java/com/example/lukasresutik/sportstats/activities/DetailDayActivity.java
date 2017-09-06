2b2a0657dcb7ba263caa8e1c1485be0f72a19b13
Lukáš Rešutík <lresutik@gmail.com>
2017-09-06 16:39:11 +0200

    Hide fab in detail day activity
    Hide fab in detail day activity

--- app/src/main/java/com/example/lukasresutik/sportstats/activities/DetailDayActivity.java
+++ app/src/main/java/com/example/lukasresutik/sportstats/activities/DetailDayActivity.java
@@ -30,13 +30,7 @@ public class DetailDayActivity extends AppCompatActivity implements WeekView.Eve
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.hide();
     }
 
     private void init() {
